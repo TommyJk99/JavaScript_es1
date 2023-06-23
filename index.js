2dbf4b13ab151103d1b3e7677a3e13aa5293bb29
ThomasMazzotta <thomasmazzotta99@gmail.com>
2023-06-23 13:23:44 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+_________________________________________________________________________________________________________________
+
+1) Cos'è un algoritmo?
+
+1) Un algoritmo è una serie di istruzioni che permettono di arrivare ad una determinato risultato.
+   Se voglio costruire un maialino di lego seguirò le istruzioni passo a passo per arrivare al risultato finale.
+   Se l'ordine o la correttezza dei passaggi non segue le istruzioni, il risultato finale sarà (generalmente) differente.
+_________________________________________________________________________________________________________________
+
+2) Cos'è una variabile?
+
+2) Una variabile è un oggetto astratto capace di assumere diversi valori (compreso il valore nullo).
+   Per quanto riguarda l'informatica la variabile è concretizzata in uno o molteplici spazi di memoria.
+   Per fare una similitudine, una variabile è come il tipico cilindro di un mago: può essere vuota o contenere
+   qualcosa a seconda del momento in cui controllo il suo interno.
+
+_________________________________________________________________________________________________________________
+
+3) Undefined e null sono la stessa cosa.
+
+3) Undefined e null non sono la stessa cosa. Una valore undefined è caratteristico di una variabile che è 
+   stata definita ma a cui non è stato ancora attribuito un valore. Un valore null fa riferimento ad una
+   variabile definita a cui è stato attribuito il valore "null". 
+__________________________________________________________________________________________________________________
+  
+*/
